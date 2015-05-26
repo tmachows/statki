5a0d3cc8a3f4c21a02d09b573c520cc98252a2df
l0rd11 <majarosz.j50@gmail.com>
2015-05-26 11:02:42 +0200

    Update serwer.c

--- serwer.c
+++ serwer.c
@@ -1,7 +1,155 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include<stdio.h>
+#include<string.h>    //strlen
+#include<stdlib.h>    //strlen
+#include<sys/socket.h>
+#include<arpa/inet.h> //inet_addr
+#include<unistd.h>    //write
+#include<pthread.h> //for threading , link with lpthread
+ 
+//the thread function
 
-int main()
+struct node{
+	int val;
+	struct node *next;
+}; 
+pthread_cond_t cond= PTHREAD_COND_INITIALIZER;
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+struct node *head=NULL;
+
+void *connection_handler(void *);
+ 
+int main(int argc , char *argv[])
 {
-	return 0;
+    int socket_desc , client_sock , c , *new_sock;
+    struct sockaddr_in server , client;
+     
+    //Create socket
+    socket_desc = socket(AF_INET , SOCK_STREAM , 0);
+    if (socket_desc == -1)
+    {
+        printf("Could not create socket");
+    }
+    puts("Socket created");
+     
+    //Prepare the sockaddr_in structure
+    server.sin_family = AF_INET;
+    server.sin_addr.s_addr = INADDR_ANY;
+    server.sin_port = htons( 8888 );
+     
+    //Bind
+    if( bind(socket_desc,(struct sockaddr *)&server , sizeof(server)) < 0)
+    {
+        //print the error message
+        perror("bind failed. Error");
+        return 1;
+    }
+    puts("bind done");
+     
+    //Listen
+    listen(socket_desc , 3);
+     
+    //Accept and incoming connection
+    puts("Waiting for incoming connections...");
+    c = sizeof(struct sockaddr_in);
+     
+     
+    //Accept and incoming connection
+    puts("Waiting for incoming connections...");
+    c = sizeof(struct sockaddr_in);
+    while( (client_sock = accept(socket_desc, (struct sockaddr *)&client, (socklen_t*)&c)) )
+    {
+        puts("Connection accepted");
+         
+        pthread_t sniffer_thread;
+        new_sock = malloc(1);
+        *new_sock = client_sock;
+         
+        if( pthread_create( &sniffer_thread , NULL ,  connection_handler , (void*) new_sock) < 0)
+        {
+            perror("could not create thread");
+            return 1;
+        }
+         
+        //Now join the thread , so that we dont terminate before the thread
+        //pthread_join( sniffer_thread , NULL);
+        puts("Handler assigned");
+    }
+     
+    if (client_sock < 0)
+    {
+        perror("accept failed");
+        return 1;
+    }
+     
+    return 0;
+}
+ 
+/*
+ * This will handle connection for each client
+ * */
+void *connection_handler(void *socket_desc)
+{
+    //Get the socket descriptor
+    int sock = *(int*)socket_desc;
+    int enymy_sock=sock;
+    int read_size;
+    char *message , client_message[2000];
+    int state=0;
+    //Send some messages to the client
+    //message = "Greetings! I am your connection handler\n";
+   // write(sock , message , strlen(message));
+     
+   // message = "Now type something and i shall repeat what you type \n";
+   // write(sock , message , strlen(message));
+     
+    //Receive a message from client
+    while(1)
+    {
+    	read_size = recv(sock , client_message , 2000 , 0);
+    	
+    	if(strstr(client_message,"start")==client_message){
+    			pthread_mutex_lock(&mutex);	
+    				struct node *tmp=malloc(sizeof(struct node));
+    				tmp->val=sock;
+    				tmp->next=head;
+    				head=tmp;
+    				//pthread_cond_signal(&cond);
+				pthread_mutex_unlock(&mutex);
+				printf("dupa");
+				while(state==0){
+					pthread_mutex_lock(&mutex);	
+						if(head!=NULL&&head->val!=sock){
+							enymy_sock=head->val;
+							head=head->next;
+							state=1;
+						}	
+					
+					
+					pthread_mutex_unlock(&mutex);
+					printf("dupa1");
+					sleep(1);
+				}			
+			write(enymy_sock , client_message , strlen(client_message));   	
+    	}
+    	else{
+    		write(enymy_sock , client_message , strlen(client_message));   		
+    	}
+    	
+        
+    }
+     
+    if(read_size == 0)
+    {
+        puts("Client disconnected");
+        fflush(stdout);
+    }
+    else if(read_size == -1)
+    {
+        perror("recv failed");
+    }
+         
+    //Free the socket pointer
+    free(socket_desc);
+     
+    return 0;
 }
