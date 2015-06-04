96d1af7e52b76ea1a4f862e945e47e2ad029c830
Vlizer <vlizer3@gmail.com>
2015-06-04 15:07:55 +0200

    Update config.h

--- config.h
+++ config.h
@@ -7,25 +7,48 @@
 
 #define CLIENT_NAME_LENGHT 50
 #define HISTORY_LENGHT 1024
-#define MAX_CLIENT 20
+#define MAX_CLIENTS 20
 
 typedef enum{
 	MENU,
 	GAME
-} lobby_t
+}lobby_t;
+
+typedef struct{
+	char msg[HISTORY_LENGHT];
+} history_request_t;
+
+typedef struct{
+	int x;
+	int y;
+}field_t;
 
 
-typedef enum {
-	START_GAME,	
-	CHECK_HISTORY,
-	DISCONNECT,
-	FIELD,
-	FIELD_STATE,
-	GAME_STATE, //WIN,LOSS,DISCONNECT
-	PLAYER_READY, // READY
+typedef enum{
+	WIN,
+	LOSS,
+	DISCON    //uwaga tutaj zmiana komunikatu bo sie powtarzaly i mi bledy lecialy przy kompilacji
+}game_state_t;
+
+
+typedef enum{
+	HIT,
+	MISS
+}field_state_t;
+
+
+typedef enum{
+	ADD_USER, //MENU wysylane przy probie polaczenia z serwerem
+	START_GAME,//MENU
+	CHECK_HISTORY,//MENU
+	DISCONNECT,//MENU,GAME
+	FIELD,//GAME
+	FIELD_STATE,//GAME
+	GAME_STATE, //GAME,WIN,LOSS,DISCONNECT
+	PLAYER_READY, //GAME READY
 	HISTORY,
 	SERVER_INFO
-} action_t;
+}action_t;
 
 typedef enum{
 	CONNECTED,
@@ -33,8 +56,9 @@ typedef enum{
 	DISCONNECTED,
 	OPPONENT
 }server_info_t;
+
 struct client_struct {
-	char name[CLIENT_NAME_LENGTH];
+	char name[CLIENT_NAME_LENGHT];
 	int socket;
 	pthread_t thread;
 	struct client_struct* next;
@@ -44,7 +68,7 @@ typedef struct client_struct client_t;
 
 
 typedef struct {
-	char name[CLIENT_NAME_LENGTH];
+	char name[CLIENT_NAME_LENGHT];
 	int opponent_socket;  // GAME.SERVER_INFO.OPPONENT_SOCKET   to w  opponent_socket_socket przeciwnika
 	lobby_t lobby;
 	action_t action;
@@ -52,29 +76,18 @@ typedef struct {
 	field_state_t field_state;
 	game_state_t game_state;
 	server_info_t server_info;
-} request_t;
+}request_t;
+struct game_struct{
+	client_t* player_1;
+	client_t* player_2;
+	int ready_player_1;
+	int ready_player_2;
+	struct game_struct* next;
+};
 
-typedef struct{
-	char msg[HISTORY_LENGHT];
-} history_request_t
-
-typedef struct{
-	int x;
-	int y;
-}field_t
-
-
-typedef enum{
-	WIN,
-	LOSS,
-	DISCONNECT
-} game_state_t
+typedef struct game_struct game_t;
 
 
-typedef enum{
-	HIT,
-	MISS
-}field_state_t
 
 
 #endif //CONFIG_H
